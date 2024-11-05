903c0cd25ad1bde31cca5224cddaabbfea654184
CristhiAn-555 <cristhianvalencia999@gmail.com>
2024-11-05 09:34:22 -0500

    resta

---
+++ src/main/java/ec/edu/espoch/resta2/Resta2.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.resta2;
+
+/**
+ *
+ * @author PERSONAL
+ */
+public class Resta2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        int num1=5;
+        int num2=10;
+        System.out.println("la resta de dos numeros");
+        System.out.println("el resultado: " + (num1 - num2));
+    }
+}
